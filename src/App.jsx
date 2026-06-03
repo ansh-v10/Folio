651ac2a71ef9f5eff2ba880eec63480c6fce4ed2
Ansh <anshverma2801@gmail.com>
2026-06-03 13:46:16 +0530

    feat: add Sanjay Sales project and switch to HashRouter for stable page reloads

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AppRouter from './router/AppRouter';
 import AnimatedBackground from './utils/AnimatedBackground';
 import './App.css';
@@ -7,12 +7,12 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
+    <HashRouter>
       <div style={{ position: 'relative', minHeight: '100vh', width: '100%' }}>
         <AnimatedBackground />
         <AppRouter />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
